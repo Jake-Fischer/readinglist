b9698a2d73b4ec25e9d219289ae93b954a9d68ab
mkhalil7625 <mkhalil7625@gmail.com>
2021-09-11 22:35:44 -0500

    added the counter program

--- main.py
+++ main.py
@@ -28,6 +28,7 @@ def create_menu():
     menu.add_option('5', 'Show All Books', show_all_books)
     menu.add_option('6', 'Change Book Read Status', change_read)
     menu.add_option('7', 'Delete a Book', delete_book)
+    menu.add_option('8','Show total Book Count', book_count)
     menu.add_option('Q', 'Quit', quit_program)
 
     return menu
@@ -68,13 +69,18 @@ def change_read():
     book.save()
     
 def delete_book():
-    
+
     try:
         book_id = ui.get_book_id()
         book = store.get_book_by_id(book_id) 
         book.delete()
     except :
         print("Error: Book Not Found")
+
+
+def book_count():
+    count = store.book_count()
+    print(f'There are {count} book(s) in the database.')
 
 def quit_program():
     ui.message('Thanks and bye!')
