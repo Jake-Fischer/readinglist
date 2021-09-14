f4b9ad37dec2683fc8ae9b73b6e1431b641a7dac
mkhalil7625 <mkhalil7625@gmail.com>
2021-09-14 17:34:56 -0500

    updated the docstring for the save function to correct the referenced functions

--- bookstore.py
+++ bookstore.py
@@ -20,11 +20,12 @@ class Book:
 
     def save(self):
         """
-        when called, Checks if an ID of a book was entered. 
-        If the user entered a velid ID, the save function will call
-        the update_book function to update the database record forthat book.
-        . If in ID was passed, it will call the add-book function to create_book
-         a new book record and assign the next avalable ID to the record 
+        When called, checks if an ID of a book was entered.
+        If the user entered a valid ID, the save function will call
+        the _update_book function to update the database record for that book.
+        If no ID was passed, it will call the _add_book function which 
+        will create a new book record and assign the next available ID to the new
+        book record. 
         """
         if self.id:
             self.bookstore._update_book(self)
